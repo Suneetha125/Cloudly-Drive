c25744c0c71680bbfbf650d409e5e8140c870ca3
Suneetha <suneetha@iitbhilai.ac.in>
2026-02-14 14:19:41 +0530

    Fixed build error: removed forgot password references

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,7 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Auth from './components/Auth';
-// import Drive from './components/Drive';
-// import ForgotPassword from './ForgotPassword';
-// // SDE Pattern: Only allows access to Drive if a token exists
-// const ProtectedRoute = ({ children }) => {
-//     const token = localStorage.getItem("token");
-//     return token ? children : <Navigate to="/" />;
-// };
-
-// function App() {
-//     return (
-//         <Router>
-//             <Routes>
-//                 {/* Login/Signup Page */}
-//                 <Route path="/" element={<Auth />} />
-
-//                 {/* Secure Drive Page */}
-//                 <Route 
-//                     path="/drive" 
-//                     element={
-//                         <ProtectedRoute>
-//                             <Drive />
-//                         </ProtectedRoute>
-//                     } 
-//                 />
-
-//                 {/* Redirect any unknown routes to Login */}
-//                 <Route path="*" element={<Navigate to="/" />} />
-//             </Routes>
-//         </Router>
-//     );
-// }
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './components/Auth';
 import Drive from './components/Drive';
-import ForgotPassword from './ForgotPassword';
 
 // SDE Pattern: Only allows access to Drive if a token exists
 const ProtectedRoute = ({ children }) => {
@@ -50,13 +13,10 @@ function App() {
     return (
         <Router>
             <Routes>
-                {/* Login/Signup Page */}
+                {/* 1. Login/Signup Page */}
                 <Route path="/" element={<Auth />} />
 
-                {/* Forgot Password Page (Public) */}
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-
-                {/* Secure Drive Page (Protected) */}
+                {/* 2. Secure Drive Page (Requires Login) */}
                 <Route 
                     path="/drive" 
                     element={
@@ -66,7 +26,7 @@ function App() {
                     } 
                 />
 
-                {/* Redirect any unknown routes to Login */}
+                {/* 3. Redirect any unknown routes to Login */}
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
